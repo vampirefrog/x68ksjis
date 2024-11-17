af0064ca319c35a48c9a5e381f9076b0aa23c263
vampi <vampi@vampi.tech>
2024-11-17 17:34:52 +0200

    add utf8_fgetc

--- utf8.c
+++ utf8.c
@@ -36,3 +36,37 @@ int utf8_fputc(int c, FILE *f) {
 	}
 	return EOF;
 }
+
+int utf8_fgetc(FILE *f) {
+	int state = 0;
+	int codepoint = 0;
+	int b = 0;
+	while((b = fgetc(f)) != EOF) {
+		if(state == 0) {
+			if (b < 0x80) {  // Single-byte (ASCII)
+				return b;
+			} else if ((b & 0xE0) == 0xC0) {  // Start of 2-byte sequence
+				codepoint = b & 0x1F;
+				state = 1;
+			} else if ((b & 0xF0) == 0xE0) {  // Start of 3-byte sequence
+				codepoint = b & 0x0F;
+				state = 2;
+			} else if ((b & 0xF8) == 0xF0) {  // Start of 4-byte sequence
+				codepoint = b & 0x07;
+				state = 3;
+			} else {
+				return -2;
+			}
+		} else {
+			if((b & 0xC0) == 0x80) {  // Valid continuation byte
+				codepoint = (codepoint << 6) | (b & 0x3F);
+				state--;  // Move to the next expected byte or finish
+				if(state == 0)
+					return codepoint;
+			} else {
+				return -2;
+			}
+		}
+	}
+	return EOF;
+}
--- utf8.h
+++ utf8.h
@@ -6,3 +6,4 @@ int utf8_encode(int c, char *buf);
 int utf8_strlen(char *buf);
 int utf8_strnlen(char *buf, int n);
 int utf8_fputc(int c, FILE *f);
+int utf8_fgetc(FILE *f);
